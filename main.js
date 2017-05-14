bc91dc98d503441695b0d99c459aa20d7fd64b66
Hurshal Patel <hurshal@imap.cc>
2017-05-13 19:36:43 -0700

    add shitty spectrogram

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ var widgets = [
     new SourceWidget(source),
     new AnalyserFrequencyWidget(analyser),
     new GainWidget(gain),
-    new AnalyserFrequencyWidget(analyserPost)
+    new SpectrogramWidget(analyserPost)
 ];
 
 //TODO fix prototype structure
@@ -123,6 +123,129 @@ AnalyserFrequencyWidget.prototype.render = function() {
         var y = v * this.HEIGHT;
 
         this.ctx.fillRect(x + 1, 0, w - 1, y);
+    }
+};
+
+var h2r = function(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+    ] : null;
+};
+
+// Inverse of the above
+var r2h = function(rgb) {
+    return "#" + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1);
+};
+
+// Interpolates two [r,g,b] colors and returns an [r,g,b] of the result
+// Taken from the awesome ROT.js roguelike dev library at
+// https://github.com/ondras/rot.js
+var _interpolateColor = function(color1, color2, factor) {
+  if (arguments.length < 3) { factor = 0.5; }
+  var result = color1.slice();
+  for (var i=0;i<3;i++) {
+    result[i] = Math.round(result[i] + factor*(color2[i]-color1[i]));
+  }
+  return result;
+};
+
+var rgb2hsl = function(color) {
+  var r = color[0]/255;
+  var g = color[1]/255;
+  var b = color[2]/255;
+
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = (l > 0.5 ? d / (2 - max - min) : d / (max + min));
+    switch(max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+    h /= 6;
+  }
+
+  return [h, s, l];
+};
+
+var hsl2rgb = function(color) {
+  var l = color[2];
+
+  if (color[1] == 0) {
+    l = Math.round(l*255);
+    return [l, l, l];
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    var s = color[1];
+    var q = (l < 0.5 ? l * (1 + s) : l + s - l * s);
+    var p = 2 * l - q;
+    var r = hue2rgb(p, q, color[0] + 1/3);
+    var g = hue2rgb(p, q, color[0]);
+    var b = hue2rgb(p, q, color[0] - 1/3);
+    return [Math.round(r*255), Math.round(g*255), Math.round(b*255)];
+  }
+};
+
+var _interpolateHSL = function(color1, color2, factor) {
+  if (arguments.length < 3) { factor = 0.5; }
+  var hsl1 = rgb2hsl(color1);
+  var hsl2 = rgb2hsl(color2);
+  for (var i=0;i<3;i++) {
+    hsl1[i] += factor*(hsl2[i]-hsl1[i]);
+  }
+  return hsl2rgb(hsl1);
+};
+
+function SpectrogramWidget(analyser) {
+    Widget.call(this, "spectrogram", 1024, 256);
+
+    this.analyser = analyser;
+    this.bufLen = analyser.frequencyBinCount;
+
+    this.retention = 256;
+    this.data = new Array(this.retention);
+    for (var i = 0; i < this.retention; i++) {
+        this.data[i] = new Uint8Array(this.bufLen);
+    }
+};
+
+SpectrogramWidget.prototype.render = function() {
+    Widget.prototype.render.call(this);
+
+    var data = this.data.shift();
+    this.analyser.getByteFrequencyData(data);
+    this.data.push(data);
+
+    var h = this.HEIGHT * 1.0 / this.bufLen;
+    var w = this.WIDTH * 1.0 / this.retention;
+
+    for (var x = 0; x < this.retention; x++) {
+        for (var y = 0; y < this.bufLen; y++) {
+            var v = this.data[x][y] / SAMPLE_MAX;
+
+            var a = h2r('#00C3FF');
+            var b = h2r('#FFFF1C');
+            var c = _interpolateColor(a, b, v);
+            this.ctx.fillStyle = r2h(c);;
+
+            this.ctx.fillRect(x * w, y * h, w, h);
+        }
     }
 };
 
